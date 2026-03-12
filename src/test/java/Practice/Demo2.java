8bcad4af818e96d9956ca7a31547fd5b3f5f0295
nirsh <nirsh@Nirshi>
2026-03-12 11:18:34 +0530

    committing pull operation to framework directory

--- src/test/java/Practice/Demo2.java
+++ src/test/java/Practice/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("practicing pull operation ");
 		System.out.println("through branch");
+		System.out.println("pull operation to framework directory ");
 	}
 
 }
